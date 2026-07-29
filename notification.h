1c58ffde48a4ff6164bee398dac3405d3e3dce63
Kaviamuthu04 <kaviamuthua@gmail.com>
2026-07-29 14:07:04 +0530

    Create notification.h

---
+++ notification.h
@@ -0,0 +1,8 @@
+#ifndef NOTIFICATION_H
+#define NOTIFICATION_H
+void sendCancellationNotification(
+        int pnr,
+        float refund);
+void sendNotification();
+void viewNotifications();
+#endif
